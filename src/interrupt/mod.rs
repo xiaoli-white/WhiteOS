7824be426b7db273209af2a1bb2a651f4414b757
xiaoli <maxljh0216@yeah.net>
2026-04-09 22:54:32 +0800

    ✨ feat(interrupt): Add keyboard interrupt handler and scancode output

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -6,12 +6,13 @@ use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 use crate::console::with_console;
-use pics::PIC_1_OFFSET;
+use pics::{PIC_1_OFFSET, PICS};
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
@@ -26,6 +27,7 @@ lazy_static! {
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
         idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer::timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_u8()].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -38,6 +40,18 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+    with_console(|console| write!(console, "{}", scancode).unwrap());
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
 }
 
 pub fn init_idt() {
